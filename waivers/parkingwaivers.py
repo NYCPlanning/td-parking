bf3dceae3fc0058cc3f77977d16c152be5ec153d
mariafree <93666624+mariafree@users.noreply.github.com>
2022-06-17 16:52:08 -0400

    update

--- waivers/parkingwaivers.py
+++ waivers/parkingwaivers.py
@@ -88,43 +88,38 @@ client = Socrata(data_link, app_token)
 # required accessory off-street parking spaces for residences when permitted in commercial districts (zr 36-30)
 # permitted off-street parking in the manhattan core (zr 13-10) and long island city area (zr 16-10)
 # requirements where group parking facilities are provided (zr 25-23)
+# applicability of (accessory off-street parking) regulations in community district 14, queens (zr 25-027)
+# applicability of (accessory off-street parking) regulations in waterfront area (zr 25-025)
 # reduced requirements for small zoning lots (zr 25-241)
 
 reslots_df = pd.read_csv(path + 'input/reslots.csv', dtype = str)
 reslots_df[['units', 'lotarea', 'lotfront']] = reslots_df[['units', 'lotarea', 'lotfront']].astype(float)
 
-# pull out residential component in mx zones 
+# convert districts from... 
+
+# ... mixed use to their residential component
 reslots_df['zonedistadj'] = reslots_df['zonedist'].str.split('/').str.get(-1) # mx zones
 
-# convert commercial districts to residential district equivalents 
+# ... commercial to residential district equivalents (zr 36-30)
 rde_df = pd.read_csv(path + 'input/resdistequiv.csv', dtype = str)
 rde_di = rde_df.set_index('commdist')['resdist'].to_dict()
 reslots_df['zonedistadj'] = reslots_df['zonedistadj'].replace(rde_di)
 
-# convert commercial overlays to the residential districts in which they are mapped
+# ... commercial overlay to the residential districts in which they are mapped (zr 36-30)
 overlay = ('C1-1', 'C1-2', 'C1-3', 'C1-4', 'C1-5', 'C2-1', 'C2-2', 'C2-3', 'C2-4', 'C2-5')
 overlay_cond = (reslots_df['zonedistadj'].str.startswith(overlay)) & (reslots_df['zonedistb'].notna())
 reslots_df['zonedistb'] = reslots_df['zonedistb'].replace(' ', np.nan)
 reslots_df.loc[overlay_cond, 'zonedistadj'] = reslots_df['zonedistb']
 
-# determine if lot is...
+# determine if lot is in ...
 
-# ... in the manhattan core or long island city area
+# ... the manhattan core or long island city area (zr 16-10)
 mnc_li = ['101','102', '103', '104', '105', '106', '107', '108'] 
-lic_li = pd.read_csv(path + 'input/lic_bbl.csv', dtype = str).loc[:,'bbl'] # gis point in polygon
+lic_li = pd.read_csv(path + 'input/licbbl.csv', dtype = str).loc[:,'bbl'] # gis point in polygon
 
 reslots_df['mnc_lic'] = (reslots_df['cd'].isin(mnc_li)) | (reslots_df['bbl'].isin(lic_li))
 
-# ... considered small 
-small10k_li = ['R6', 'R7-1', 'R7A', 'R7B', 'R7D', 'R7X']
-small15k_li = ['R7-2', 'R8', 'R9', 'R10']
-
-small10k_cond = (reslots_df['lotarea'] <= 10000) & (reslots_df['zonedistadj'].isin(small10k_li))
-small15k_cond = (reslots_df['lotarea'].between(10001, 15000)) & (reslots_df['zonedistadj'].isin(small15k_li)) & (reslots_df['zonedistadj'] != 'R8B')
-
-reslots_df['small'] = small10k_cond | small15k_cond
-
-# ... in a lower density growth management area
+# ... a lower density growth management area (zr 25-23)
 ldgma_si = ('R1', 'R2','R3','R4A', 'R4-1', 'C1', 'C2', 'C3A', 'C4')
 ldgma_bx10 = ('R1', 'R2','R3','R4A', 'R4-1', 'R6', 'R7', 'C1', 'C2', 'C3A')   
 
@@ -133,10 +128,34 @@ ldgma_bx10_cond = (reslots_df['cd'] == '210') & (reslots_df['zonedistadj'].str.s
 
 reslots_df['ldgma'] = ldgma_si_cond | ldgma_bx10_cond
 
+# adjust district name if governed by the regulations of another, including lots in a ...
+
+# ... brooklyn R8B (zr 25-23)
+bk_r8_cond = (reslots_df['cd'].str.startswith('3')) & (reslots_df['zonedistadj'] == 'R8B')
+reslots_df.loc[bk_r8_cond, 'zonedistadj'] = 'R8'
+
+# ... queens cd14 R6 or R7, but not in the adverne or edgemere urban renewal areas (zr 25-027)
+ura_li = pd.read_csv(path + 'input/urabbl.csv', dtype = str).loc[:,'bbl'] # gis point in polygon
+ura_cond = (reslots_df['cd'] == '414') & (~reslots_df['bbl'].isin(ura_li)) & (reslots_df['zonedistadj'].str.startswith(('R6', 'R7')))
+reslots_df.loc[ura_cond,'zonedistadj'] = 'R5'
+
+# ... waterfront area (zr 25-025)
+wtr_cond = (reslots_df['lottype'] == '2') & (reslots_df['zonedistadj'] == 'R7-3')
+reslots_df.loc[wtr_cond, 'zonedistadj'] = 'R7-2'
+
+# determine if lot is considered small (zr 25-241)
+small10k_li = ['R6', 'R7-1', 'R7A', 'R7B', 'R7D', 'R7X']
+small15k_li = ['R7-2', 'R8', 'R9', 'R10']
+
+small10k_cond = (reslots_df['lotarea'] <= 10000) & (reslots_df['zonedistadj'].isin(small10k_li))
+small15k_cond = (reslots_df['lotarea'].between(10001, 15000)) & (reslots_df['zonedistadj'].isin(small15k_li)) & (reslots_df['zonedistadj'] != 'R8B')
+
+reslots_df['small'] = small10k_cond | small15k_cond
+
 #%% Effective Parking: Spaces Required
 
 # import standard and small lot parking requirements
-reqpark_df = pd.read_csv(path + 'input/requiredparking.csv')
+reqpark_df = pd.read_csv(path + 'input/reqpark.csv')
 
 # set variables for lots not counted
 # c_notcounted = 0 # commercial
@@ -154,13 +173,16 @@ reqpark_df = pd.read_csv(path + 'input/requiredparking.csv')
 def get_required_parking(row):
     if (row['zonedistadj'] in list(reqpark_df['zonedist'])) is False:
         multiplier = 0
-
     elif row['mnc_lic'] is True: 
         multiplier = 0
     elif row['small'] is True:
-        multiplier =  reqpark_df.loc[reqpark_df['zonedist'] == row['zonedistadj'], 'small'].values[0]
+        ldgma_bx10_small_R71_cond = (row['cd'] == '210') & (row['zonedistadj'] == 'R7-1') &  (row['lotarea'] <= 10000) # zr 25-241
+        if ldgma_bx10_small_R71_cond is True:
+            multiplier = .5
+        else:
+            multiplier =  reqpark_df.loc[reqpark_df['zonedist'] == row['zonedistadj'], 'small'].values[0]
     elif row['ldgma'] is True:
-        if row['zonedist'].startswith(('C1', 'C2')):
+        if row['zonedist'].startswith(('C1', 'C2')): # zr 36-321
             multiplier = 1
         else:
             multiplier = 1.5
@@ -170,6 +192,7 @@ def get_required_parking(row):
 
 reslots_df['reqpark'] = reslots_df.apply(lambda row: get_required_parking(row), axis = 1)
 reslots_df['reqspaces'] = reslots_df['units'] * reslots_df['reqpark']
+
 #%% Effective Parking: Spaces Waived 
  
 # waiver of requirements for small zoning lots in high bulk districts (zr 25-242)
